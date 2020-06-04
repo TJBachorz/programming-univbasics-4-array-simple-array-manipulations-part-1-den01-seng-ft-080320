84ebd5a7fa9e288630a75bda459e944299f882c5
Thomas Bachorz <tjbachorz@gmail.com>
2020-06-04 18:23:34 +0000

    Automatically backed up by Learn

--- spec/intro_to_simple_array_manipulations_spec.rb
+++ spec/intro_to_simple_array_manipulations_spec.rb
@@ -58,9 +58,8 @@ end
 describe "using_shift" do 
   before(:each) do 
     @my_favorite_cities = ["Lagos", "Cape Town", "Nairobi", "San Francisco", "Gaborone", "New York", "Berlin", "London"]
-    @im_so_over_this_city = using_shift(@my_favorite_cities)
     binding.pry
-    puts 'hello'
+    @im_so_over_this_city = using_shift(@my_favorite_cities)
   end
   it "takes in an argument of an array and uses the shift method to remove the first item and return it" do 
     expect(@im_so_over_this_city).to eq("Lagos")
